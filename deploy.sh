a7528e3b807e0dfa7bf3b2743424d352ee804d21
jabberjayjay <jabberjayjay@gmail.com>
2024-03-16 23:39:45 +0000

    added pem

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ cd ExampleApp/
 # Install application dependencies
 npm install
 # Start the application with the process name example_app using pm2 
-pm2 start ./bin/www --name example_app
+pm start ./bin/www --name example_app
