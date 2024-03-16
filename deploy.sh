99ae336e5a2f5f92bcacc87a706b5e4152aa48e7
jabberjayjay <jabberjayjay@gmail.com>
2024-03-16 16:38:06 +0000

    deploy sh messing

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer. 
 sudo npm install -g pm2
 # stop any instance of our application running currently
-pm2 stop example_app
+pm2 stop ExampleApplication
 # change directory into folder where application is downloaded
 cd ExampleApplication/
 # Install application dependencies
 npm install
 # Start the application with the process name example_app using pm2 
-pm2 start ./bin/www--name example_app
+pm2 start ./bin/www--name ExampleApplication
