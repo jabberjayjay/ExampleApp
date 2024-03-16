1ed1aad82eb3c2043f5b1fa7f8d9744546a0ffe6
jabberjayjay <jabberjayjay@gmail.com>
2024-03-16 23:33:28 +0000

    changed directory for sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer. 
 sudo npm install -g pm2 
 # stop any instance of our application running currently
-pm2 stop ExampleApplication
+pm2 stop ExampleApp
 # change directory into folder where application is downloaded
 cd ExampleApp/
 # Install application dependencies
